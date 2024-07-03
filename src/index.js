0321619237393689362522549464f394fd46faf6
Jahanzaib0070 <mohammadjahanzaib007@gmail.com>
2024-07-03 18:01:01 +0500

    Digital clock

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import App1 from "./App1";
 import "./index.css";
 
 ReactDOM.render(
-  <App /> , document.getElementById("root")
+  <App1 /> , document.getElementById("root")
 );
