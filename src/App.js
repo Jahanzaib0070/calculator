ed15809412731d51fe37a08e955b66ab528d00be
Jahanzaib0070 <mohammadjahanzaib007@gmail.com>
2024-07-06 02:48:18 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
 import React, { useState } from "react";
 
 const App = () => {
-  
+  //setting value for First Name
   const [name, setName] = useState("");
-
   const newText = (event)=>{
-    setName(event.target.value)
-  }  
-  
-  const [fullName, setFullname] = useState("");
-
-  const saveText = ()=>{
-    setFullname(name)
+    setName(event.target.value);
   }
-
-
+  const [firstName, setFirstname] = useState("");
   
+  //setting value for Last Name
+  const [lName, setLname] = useState("")
+  const newTexttwo = (event)=>{
+    setLname(event.target.value);
+  }
+  const [lastName, setLastname] = useState("");
+
+  //passing values to both first and last name
+  const saveText = (event)=>{
+    event.preventDefault();
+    setFirstname(name);
+    setLastname(lName);
+  }
   return(
     <>
-      <div>
-        <h1 id="text1">Hello {fullName}</h1>
-        <input className="inputText" type="text" placeholder="Enter Your Name" onChange={newText} value={name}/>
-        <button onClick={saveText}>Save</button>
-      </div>
+      <form onSubmit ={saveText}>
+        <div>
+          <h1 id="text1">Hello {`${firstName} ${lastName}`}</h1>
+          <input className="inputText" type="text" placeholder="Enter Your First Name" onChange={newText} value={name}/>
+          <br />
+          <input className="inputText" type="text" placeholder="Enter Your Last Name" onChange={newTexttwo} value={lName}/>
+          <button type="submit" >Submit</button>
+        </div>
+      </form>
     </>
   );
 }
