d4812a0484ea172ecb750ba8acf955777ea66a8a
Jahanzaib0070 <mohammadjahanzaib007@gmail.com>
2024-07-06 01:50:37 +0500

    Controlled-unconrolled components in input field

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React, { useState } from "react";
 
 const App = () => {
+  
+  const [name, setName] = useState("");
 
-  const [count , setCount] = useState(0);
+  const newText = (event)=>{
+    setName(event.target.value)
+  }  
+  
+  const [fullName, setFullname] = useState("");
 
-  const incNum = () => {
-    setCount(count + 1);
-  };
-    return(
-      <>
-        <h1>{count}</h1>
-        <button onClick={incNum}>Click Me</button>
-      </>
-    );
+  const saveText = ()=>{
+    setFullname(name)
+  }
+
+
+  
+  return(
+    <>
+      <div>
+        <h1 id="text1">Hello {fullName}</h1>
+        <input className="inputText" type="text" placeholder="Enter Your Name" onChange={newText} value={name}/>
+        <button onClick={saveText}>Save</button>
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
-import App1 from "./App1";
-import App2 from "./App2";
-import App3 from "./App3";
 import "./index.css";
 
-ReactDOM.render(
-  <App3 /> , document.getElementById("root")
-);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
