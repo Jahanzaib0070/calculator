3fdf7219efe52db6b54af5f620873dce376e5774
Jahanzaib0070 <mohammadjahanzaib007@gmail.com>
2024-07-07 00:42:18 +0500

    Update App1.jsx

--- src/App1.jsx
+++ src/App1.jsx
@@ -2,9 +2,10 @@ import React, { useState } from "react";
 
 const App1 = () => {
 
-    const [fullName, setFullName] = useState({
+    const [details, setDetails] = useState({
         fname: "",
         lname: "",
+        gender: "",
         email: "",
         phoneNum: ""
     });
@@ -14,36 +15,41 @@ const App1 = () => {
         const value = event.target.value;
         const name = event.target.name;
 
-        setFullName((prevValue)=>{
-            if(name === "fName"){
-                return{
-                    fname: value,
-                    lname: prevValue.lname,
-                    email: prevValue.email,
-                    phoneNum: prevValue.phoneNum
-                }
-            }else if(name === "lName"){
-                return{
-                    fname: prevValue.fname,
-                    lname: value,
-                    email: prevValue.email,
-                    phoneNum: prevValue.phoneNum
-                }
-            }else if(name === "Email"){
-                return{
-                    fname: prevValue.fname,
-                    lname: prevValue.lname,
-                    email: value,
-                    phoneNum: prevValue.phoneNum
-                }
-            }else if(name === "phoneNumber"){
-                return{
-                    fname: prevValue.fname,
-                    lname: prevValue.lname,
-                    email: prevValue.email,
-                    phoneNum: value
-                }
+        setDetails((prevValue)=>{
+
+            return{
+                ...prevValue,
+                [name]: value
             }
+            // if(name === "fName"){
+            //     return{
+            //         fname: value,
+            //         lname: prevValue.lname,
+            //         email: prevValue.email,
+            //         phoneNum: prevValue.phoneNum
+            //     }
+            // }else if(name === "lName"){
+            //     return{
+            //         fname: prevValue.fname,
+            //         lname: value,
+            //         email: prevValue.email,
+            //         phoneNum: prevValue.phoneNum
+            //     }
+            // }else if(name === "Email"){
+            //     return{
+            //         fname: prevValue.fname,
+            //         lname: prevValue.lname,
+            //         email: value,
+            //         phoneNum: prevValue.phoneNum
+            //     }
+            // }else if(name === "phoneNumber"){
+            //     return{
+            //         fname: prevValue.fname,
+            //         lname: prevValue.lname,
+            //         email: prevValue.email,
+            //         phoneNum: value
+            //     }
+            // }
         })
     }
 
@@ -56,15 +62,17 @@ const App1 = () => {
         <div className="main">
         <form onSubmit ={saveText}>
             <div className="form1">
-                <h1>{`Hello! ${fullName.fname} ${fullName.lname}`}</h1>
-                <h4>{`Contact: ${fullName.email} | ${fullName.phoneNum}`}</h4>
-                <input className="inputText" type="text" placeholder="Enter Your First Name" name="fName" onChange={newText} value={fullName.fname}/>
+                <h1>{`Hello! ${details.fname} ${details.lname} ${details.gender}`}</h1>
+                <h4>{` ${details.email} ${details.phoneNum}`}</h4>
+                <input className="inputText" type="text" placeholder="Enter Your First Name" name="fname" onChange={newText} value={details.fname}/>
                 <br />
-                <input className="inputText" type="text" placeholder="Enter Your Last Name" name="lName" onChange={newText} value={fullName.lname}/>
+                <input className="inputText" type="text" placeholder="Enter Your Last Name" name="lname" onChange={newText} value={details.lname}/>
                 <br />
-                <input className="inputText" type="text" placeholder="Enter Your Email" name="Email" onChange={newText} value={fullName.email}/>
+                <input className="inputText" type="text" placeholder="Enter Your gender" name="gender" onChange={newText} value={details.gender}/>
                 <br />
-                <input className="inputText" type="text" placeholder="Enter Your Phone Number" name="phoneNumber" onChange={newText} value={fullName.phoneNum}/>
+                <input className="inputText" type="text" placeholder="Enter Your Email" name="email" onChange={newText} value={details.email}/>
+                <br />
+                <input className="inputText" type="text" placeholder="Enter Your Phone Number" name="phoneNum" onChange={newText} value={details.phoneNum}/>
                 <button type="submit" >Submit</button>
             </div>
             </form>
